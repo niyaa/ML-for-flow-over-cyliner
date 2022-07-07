df2ebf75e740677910de2023b140f5a8d6ae5212
niyaa <nikeshspartan@gmail.com>
2022-07-07 16:37:24 +0200

    added extractor

--- extract-data-from-vtu.py
+++ extract-data-from-vtu.py
@@ -1,1 +1,39 @@
-import
+import meshio
+from glob import glob
+import numpy as np
+import os
+
+FilePath = 'D:\OneDrive - Politechnika Warszawska\cylinder_data'
+
+os.chdir(FilePath)
+
+ReList = glob("*/");
+
+cwd = os.getcwd()
+
+for i in ReList:
+    os.chdir(cwd)
+    print(i)
+    os.chdir(i)
+
+    Re = int(i.split(".")[0])
+
+    vtu_file_List = glob("*.vtu")
+
+    for j in vtu_file_List:
+        mesh = meshio.read(j)
+
+        u = mesh.point_data.__getitem__("u")
+        v = mesh.point_data.__getitem__("v")
+
+        uv = np.column_stack( (u,v ) )
+
+        del u, v
+
+        fileIndex = int(j.split(".")[0].split("_")[1]) 
+
+        np.savetxt( "ml-Re-"+str(Re)+"-fileIndex-"+str(fileIndex), uv)       
+
+ 
+    
+
